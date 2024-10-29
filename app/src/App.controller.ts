c0a240c4f2b90680aaa16f57603f2ed5889e0075
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-28 22:52:51 -0500

    formated table numbers to feature percentage sign suffix

--- app/src/App.controller.ts
+++ app/src/App.controller.ts
@@ -9,7 +9,7 @@ export enum GeographyName {
 	"Saint Louis" = "Saint Louis",
 }
 
-export interface TableRow {
+export interface RawRow {
 	index: string;
 	"All Workers": number;
 	"All Frontline Industries": number;
@@ -21,6 +21,18 @@ export interface TableRow {
 	"Childcare & Social Services": number;
 }
 
+export interface TableRow {
+	index: string;
+	"All Workers": string | number;
+	"All Frontline Industries": string | number;
+	"Grocery, Convenience, & Drug Stores": string | number;
+	"Public Transit": string | number;
+	"Trucking, Warehouse, & Postal Service": string | number;
+	"Building Cleaning Services": string | number;
+	"Health Care": string | number;
+	"Childcare & Social Services": string | number;
+}
+
 export const map_summary = {
 	[GeographyName.Missouri]:
 		"Taney County has the highest Frontline rate (73.92%). Unemployment rates increase as you move south east.",
@@ -30,15 +42,63 @@ export const map_summary = {
 		"Jersey County has the lowest Frontline rate (17.88%).",
 };
 
-export const createRowsByCategory = (data: TableRow[]) => ({
-	Totals: [data[0]],
-	Miscellaneous: [data[1], data[2], data[3], data[4], data[5]],
-	"Full/Part-time": [data[6], data[7]],
-	"Race/Ethnicity": [data[8], data[9], data[10], data[11], data[12]],
-	"Education Level": [data[13], data[14], data[15], data[16], data[17]],
-	"Compensation and Benefits": [data[18], data[19], data[20]],
-	"Family Responsibilities": [data[21], data[22]],
-});
+const format = (value: number) => `${value.toFixed(2)}%`;
+
+export const createRowsByCategory = (data: RawRow[]) => {
+	const formattedRows = data.map((row) => {
+		if (row.index === "All Workers (16+)") return row;
+
+		const formattedRow: TableRow = {
+			index: row.index,
+			"All Workers": format(row["All Workers"]),
+			"All Frontline Industries": format(row["All Frontline Industries"]),
+			"Grocery, Convenience, & Drug Stores": format(
+				row["Grocery, Convenience, & Drug Stores"]
+			),
+			"Public Transit": format(row["Public Transit"]),
+			"Trucking, Warehouse, & Postal Service": format(
+				row["Trucking, Warehouse, & Postal Service"]
+			),
+			"Building Cleaning Services": format(row["Building Cleaning Services"]),
+			"Health Care": format(row["Health Care"]),
+			"Childcare & Social Services": format(row["Childcare & Social Services"]),
+		};
+
+		return formattedRow;
+	});
+
+	return {
+		Totals: [formattedRows[0]],
+		Miscellaneous: [
+			formattedRows[1],
+			formattedRows[2],
+			formattedRows[3],
+			formattedRows[4],
+			formattedRows[5],
+		],
+		"Full/Part-time": [formattedRows[6], formattedRows[7]],
+		"Race/Ethnicity": [
+			formattedRows[8],
+			formattedRows[9],
+			formattedRows[10],
+			formattedRows[11],
+			formattedRows[12],
+		],
+		"Education Level": [
+			formattedRows[13],
+			formattedRows[14],
+			formattedRows[15],
+			formattedRows[16],
+			formattedRows[17],
+		],
+		"Compensation and Benefits": [
+			formattedRows[18],
+			formattedRows[19],
+			formattedRows[20],
+		],
+		"Family Responsibilities": [formattedRows[21], formattedRows[22]],
+	};
+};
 
 export const tables = {
 	[GeographyName.Missouri]: {
