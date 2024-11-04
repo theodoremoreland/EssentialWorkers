d553ad05f90d61b9464e00913ab5f4e92f0b1f55
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-03 23:12:34 -0600

    updated styling of navbar without logo and replaced mui with custom css

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -2,25 +2,23 @@
 import { ReactElement } from "react";
 
 // Material UI
-import { Toolbar, AppBar, Button } from "@mui/material";
+import { Button } from "@mui/material";
 
 // Styles
 import "./Navbar.css";
 
 const Navbar = (): ReactElement => {
     return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Button className="nav-button">
-                    <a
-                        id="download"
-                        href="https://github.com/stlrda/essential-workers/blob/master/data/stlrda-essential-workers-data.zip?raw=true"
-                    >
-                        Download Data
-                    </a>
-                </Button>
-            </Toolbar>
-        </AppBar>
+        <nav id="app-nav">
+            <Button className="nav-button">
+                <a
+                    id="download"
+                    href="https://github.com/stlrda/essential-workers/blob/master/data/stlrda-essential-workers-data.zip?raw=true"
+                >
+                    Download Data
+                </a>
+            </Button>
+        </nav>
     );
 };
 
