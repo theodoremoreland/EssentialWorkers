b5b26ffbe1bb9debb19c17428e3efb5a19c26234
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-04 00:01:09 -0600

    updated footer without logos

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -8,7 +8,15 @@ const Footer = (): ReactElement => {
     return (
         <footer>
             <p>
-                The Essential Workers project was originally developed by the{" "}
+                The{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/stlrda/essential-workers"
+                >
+                    Essential Workers project
+                </a>{" "}
+                was originally developed by the{" "}
                 <a
                     target="_blank"
                     rel="noopener noreferrer"
@@ -16,14 +24,21 @@ const Footer = (): ReactElement => {
                 >
                     St. Louis Regional Data Alliance{" "}
                 </a>
-                . The data used in this project was collected from the Census
-                Bureau (
+                in partnership with{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="http://daugherty.com"
+                >
+                    Daugherty Business Solutions
+                </a>
+                . The data used was collected from the Census Bureau (
                 <a
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://data.census.gov/mdat/#/search?ds=ACSPUMS5Y2018"
                 >
-                    Amercian Community Survey 5-year Estimates Public Use
+                    American Community Survey 5-year Estimates Public Use
                     Microdata Sample 2018
                 </a>
                 ) and the Bureau of Economic Analysis (
@@ -34,8 +49,15 @@ const Footer = (): ReactElement => {
                 >
                     GDP and Personal Income 2018
                 </a>
-                ). All of the data collected can be downloaded by clicking on
-                the "Download Data" button to the right of the navigation bar.
+                ).{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/stlrda/essential-workers/blob/master/data/stlrda-essential-workers-data.zip?raw=true"
+                >
+                    Download the data collected
+                </a>
+                .
             </p>
         </footer>
     );
