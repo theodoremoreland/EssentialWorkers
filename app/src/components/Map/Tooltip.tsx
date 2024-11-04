50be58a8979d7c37c433f19ee769f5b1c1abf73c
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-03 21:19:40 -0600

    messing with spaces vs tabs of auto formatter

--- app/src/components/Map/Tooltip.tsx
+++ app/src/components/Map/Tooltip.tsx
@@ -6,53 +6,63 @@ import { GeoJSONFeature } from "mapbox-gl";
 
 // Utils
 import {
-	formatAsLocaleNumber,
-	formatAsPercentage,
-	formatAsCurrency,
+    formatAsLocaleNumber,
+    formatAsPercentage,
+    formatAsCurrency,
 } from "../../utils";
 
 // Styles
 import "./Tooltip.css";
 
 interface Props {
-	feature: GeoJSONFeature;
+    feature: GeoJSONFeature;
 }
 
 const Tooltip = ({ feature }: Props): ReactElement => {
-	const { layer } = feature;
-	const hidden: boolean = layer?.id === "root-layer" ? false : true;
+    const { layer } = feature;
+    const hidden: boolean = layer?.id === "root-layer" ? false : true;
 
-	return (
-		<div hidden={hidden}>
-			{feature.properties && (
-				<>
-					<div>County: {feature.properties.NAME}</div>
-					<div>
-						GDP:{" "}
-						{formatAsCurrency(feature.properties["GDP (Thousands of dollars)"])}
-					</div>
-					<div>
-						Labor Force:{" "}
-						{formatAsLocaleNumber(feature.properties["Labor Force"])}
-					</div>
-					<div>
-						Unemployment:{" "}
-						{formatAsPercentage(feature.properties["Unemployment Rate"])}
-					</div>
-					<div>
-						Median Income:{" "}
-						{formatAsCurrency(
-							feature.properties["Median Income Essential Workers"]
-						)}
-					</div>
-					<div>
-						Frontline Rate:{" "}
-						{formatAsPercentage(feature.properties["Frontline Industry Rate"])}
-					</div>
-				</>
-			)}
-		</div>
-	);
+    return (
+        <div hidden={hidden}>
+            {feature.properties && (
+                <>
+                    <div>County: {feature.properties.NAME}</div>
+                    <div>
+                        GDP:{" "}
+                        {formatAsCurrency(
+                            feature.properties["GDP (Thousands of dollars)"]
+                        )}
+                    </div>
+                    <div>
+                        Labor Force:{" "}
+                        {formatAsLocaleNumber(
+                            feature.properties["Labor Force"]
+                        )}
+                    </div>
+                    <div>
+                        Unemployment:{" "}
+                        {formatAsPercentage(
+                            feature.properties["Unemployment Rate"]
+                        )}
+                    </div>
+                    <div>
+                        Median Income:{" "}
+                        {formatAsCurrency(
+                            feature.properties[
+                                "Median Income Essential Workers"
+                            ]
+                        )}
+                    </div>
+                    <div>
+                        Frontline Rate:{" "}
+                        {formatAsPercentage(
+                            feature.properties["Frontline Industry Rate"]
+                        )}
+                    </div>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Tooltip;
