5d89f41db48cb25fa19d7c9d6d9595cc2d1e4cf2
Theodore Moreland <theodoremoreland@yahoo.com>
2025-08-04 12:03:50 -0500

    added toggle legend button to map legend and now default to a closed legend

--- app/src/components/Map/Legend.tsx
+++ app/src/components/Map/Legend.tsx
@@ -1,7 +1,8 @@
 // React
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
 // Material UI
+import ChevronRight from '@mui/icons-material/ChevronRight';
 import StopIcon from '@mui/icons-material/Stop';
 
 // Styles
@@ -20,21 +21,43 @@ const Legend = (props: Props): ReactElement => {
     const { legendObj } = props;
     const { stops, description, stopLabels, palette } = legendObj;
 
+    const [isLegendOpen, setIsLegendOpen] = useState<boolean>(false);
+
     return (
         <div id="legend-area-container">
             <div id="legend-area">
-                <label className="legend-label">{description}</label>
-                {stops.map((_, index: number) => (
-                    <div key={stopLabels[index]} className="legend">
-                        <div key={stopLabels[index]} className="legend-values">
-                            {stopLabels[index]}
-                        </div>{' '}
-                        <StopIcon
-                            className="legend-colors"
-                            style={{ color: palette[index] }}
-                        />
-                    </div>
-                ))}
+                <button
+                    className={`legend-button ${
+                        isLegendOpen ? 'open' : 'closed'
+                    }`}
+                    onClick={() => setIsLegendOpen(!isLegendOpen)}
+                    aria-label="Toggle Legend"
+                >
+                    <span>{isLegendOpen ? 'Hide Legend' : 'Show Legend'}</span>
+                    <ChevronRight
+                        sx={{ fontSize: '1.3em' }}
+                        className="legend-chevron"
+                    />
+                </button>
+                {isLegendOpen && (
+                    <>
+                        <label className="legend-label">{description}</label>
+                        {stops.map((_, index: number) => (
+                            <div key={stopLabels[index]} className="legend">
+                                <div
+                                    key={stopLabels[index]}
+                                    className="legend-values"
+                                >
+                                    {stopLabels[index]}
+                                </div>{' '}
+                                <StopIcon
+                                    className="legend-colors"
+                                    style={{ color: palette[index] }}
+                                />
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
         </div>
     );
